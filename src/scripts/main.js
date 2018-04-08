40a95ce8656616a03408cf837391cb33f1b48b79
ericvasconcelos <eric_vasconcelosoliveira@hotmail.com>
2018-04-08 20:28:18 -0300

    Created perspective page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,6 +14,39 @@ $(document).ready( function(){
 
 $(function(){
   $('.modal__close').on('click', function() {
-    $(this).closest('#modal').fadeOut(300);
+    $(this).closest('.js-modal').fadeOut(300);
+  })
+
+  $('.location__filter__item').on('click', function() {
+    $('.location__filter__item').removeClass('active')
+    $(this).addClass('active')
+    $('.js-map').attr('src', `./images/mapa-${$(this).data('filter')}.jpg`)
+  })
+
+  // Perspective
+  $('.perspective__video').on('click', function() {
+    $('.js-modal').fadeIn(300);
+  })
+
+  $('.perspective__carousel').owlCarousel({
+    items: 1,
+    loop: false,
+    margin: 0,
+    nav: true,
+    dots: false,
+    onChanged(event) {
+      $('.carousel-dot').removeClass('active')
+      $('.carousel-dot').eq(event.item.index).addClass('active')
+    }
+  })
+
+  $('.carousel-dot').on('click', function() {
+    $('.carousel-dot').removeClass('active')
+    const $this = $(this)
+    const carousel = $('.perspective__carousel').data('owl.carousel');
+    const index = $this.data('index')
+
+    $this.addClass('active')
+    carousel.to(index)
   })
 });
